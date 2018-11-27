b5f421ac55e876898e10b2b15bb396779e62e2e8
athatheocsd <athatheo@csd.auth.gr>
2018-11-27 23:39:40 +0200

    First Commit

---
+++ cmake-build-debug/CMakeFiles/Convolution.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Convolution.dir/main.cpp.o"
+  "Convolution.pdb"
+  "Convolution"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Convolution.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
